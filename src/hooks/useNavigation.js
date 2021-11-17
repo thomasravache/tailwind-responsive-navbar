77cc302165b217fd74c7d9f687a15e8951660977
thomasravache <thomasravache31@gmail.com>
2021-11-17 19:22:14 -0300

    Atualizando hook usenavigation

--- src/hooks/useNavigation.js
+++ src/hooks/useNavigation.js
@@ -1,7 +1,9 @@
 import { useState, useCallback } from 'react';
+import navigationElements from '../data/navigation';
 
 const useNavigation = () => {
-  const [route, setRoute] = useState('Home');
+  const [firstNavigationElement] = navigationElements;
+  const [route, setRoute] = useState(firstNavigationElement);
 
   // O hook usecallback serve para questão de performance, poderia ter sido usada uma function comum
   // mas o usecallback serve para que a função não seja recriada desnecessariamente sempre que o componente em questão for chamado, somente sendo recriada caso seja necessario
