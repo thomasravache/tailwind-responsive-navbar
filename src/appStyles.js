e67ff0b42ac03ba2f9c1e9e1a981de281701eb7e
thomasravache <thomasravache31@gmail.com>
2021-11-20 00:51:43 -0300

    Ajustando estilos

---
+++ src/appStyles.js
@@ -0,0 +1,6 @@
+const styles = {
+  container: 'bg-gray-200 dark:bg-black dark:bg-opacity-50 h-screen',
+  devLogo: 'flex items-center justify-center text-5xl text-gray-300 h-5/6 dark:text-gray-200',
+};
+
+export default styles;
---
+++ src/components/tailwindStyles/tabbarStyles.js
@@ -0,0 +1,8 @@
+const styles = {
+  tabbar: 'flex md:hidden flex-row items-center justify-between px-8 h-20 bg-white dark:bg-black visible md:invisible fixed bottom-0 w-full rounded-t-3xl text-2xl',
+  tabItem: 'text-gray-400 dark:text-gray-400 dark:hover:text-gray-300 hover:text-gray-700 cursor-pointer w-20 h-full flex items-center justify-center',
+  tabItemActive: 'bg-gradient-to-t from-white dark:from-black dark:to-blue-300 to-gray-100 border-t-4 border-gray-700 dark:border-blue-300 text-gray-700',
+  icon: '-mb-1',
+};
+
+export default styles;
